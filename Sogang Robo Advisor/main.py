eaec82b31e01d49824eff6849b995c4a2dd97f68
JUNWOO GWAK <lunarsabbth@gmail.com>
2024-11-27 10:39:58 +0900

    Chg Bench

--- Sogang Robo Advisor/main.py
+++ Sogang Robo Advisor/main.py
@@ -93,6 +93,9 @@ def main(codes, risk_level: int = 4, investor_goal:int = 1):
     tree = build_investment_tree(codes, risk_level, universe)
 
     assets = tree.get_all_nodes_name()
+
+    if '069500' not in assets: assets.append('069500')
+
     price_data = fetch_data_from_db(assets)
 
     assumption = AssetAssumption(returns_window=52, covariance_window=52)
@@ -143,7 +146,7 @@ def main(codes, risk_level: int = 4, investor_goal:int = 1):
     allocation = {name: allocation[code] for name, code in stock_dict.items() if code in allocation}
 
     eval_metrix = backtest.evaluation(allocation)
-    print(eval_metrix)
+    # print(eval_metrix)
     return eval_metrix
 
 
@@ -153,4 +156,4 @@ codes: 투자할 종목 리스트
 risk_level: 투자자의 위험등급 (1~5, 5: 고위험 투자자)
 investor_goal: 투자자 목표(1: 목돈마련, 2: 결혼자금 준비, 3: 노후자금 준비, 4: 장기수익 창출)
 """
-main(codes=['069500','139260','161510','273130','439870','251340','114260'], risk_level=5, investor_goal=4)
+# main(codes=['139260','161510','273130','439870','251340','114260'], risk_level=5, investor_goal=4)
