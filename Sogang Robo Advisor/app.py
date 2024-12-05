1d0cf1d76b6645fcfe777ef381c1f528391f42b0
CodePsy-2001 <codepsy2001@gmail.com>
2024-12-05 11:18:16 +0900

    maxLength 제거

--- Sogang Robo Advisor/app.py
+++ Sogang Robo Advisor/app.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel, Field, conlist
+from pydantic import BaseModel, Field
+from typing import List
+
 from main import main as advisor
 
 
 class RequestBody(BaseModel):
-    codes: conlist(str, max_length=300)  # list of str, max length 300
+    codes: List[str]
     risk_level: int = Field(..., ge=1, le=5)  # 1 <= risk_level <= 5
     investor_goal: int = Field(..., ge=1, le=4)  # 1 <= investor_goal <= 4
 
