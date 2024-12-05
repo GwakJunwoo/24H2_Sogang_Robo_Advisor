357a02aeb7f3e4ee0a48375e19835e6806fcbd5f
CodePsy-2001 <codepsy2001@gmail.com>
2024-12-05 10:52:56 +0900

    add cors

--- Sogang Robo Advisor/app.py
+++ Sogang Robo Advisor/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, Field, conlist
 from main import main as advisor
 
@@ -10,6 +11,14 @@ class RequestBody(BaseModel):
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 모든 도메인 허용. 필요에 따라 특정 도메인으로 제한.
+    allow_credentials=True,
+    allow_methods=["*"],  # 모든 HTTP 메서드 허용.
+    allow_headers=["*"],  # 모든 HTTP 헤더 허용.
+)
 
 
 @app.get("/healthcheck")
